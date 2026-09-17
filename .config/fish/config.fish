797d8308f3fe862d47b84768ede870a286e8bfcf
zlimon <zlobniy.limon@gmail.com>
2026-09-17 16:20:00 +0300

    remove fish greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fish_greeting ""
+
 if test -e /opt/homebrew/bin/brew 
     eval "$(/opt/homebrew/bin/brew shellenv)"
 end
